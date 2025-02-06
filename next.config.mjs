2d7e0d6277182f3162eeb1a5592171e2e7077bec
Christopher Okafor <chrisoxygen1234@gmail.com>
2025-02-06 21:44:21 +0100

    show console logs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV === "production",
+  // },
 };
 
 export default nextConfig;
